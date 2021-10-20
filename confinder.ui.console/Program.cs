026ab3f21d940826012827d9c690c29c2687dbdc
Daniel Varoto <daniel.varoto@loft.com.br>
2021-10-20 19:33:34 -0300

    intial structure

---
+++ confinder.ui.console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace confinder.ui.console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
