d02cccc76398b4797bf4678f6ade466a285166e2
Daniel Varoto <daniel.varoto@gmail.com>
2022-12-08 10:01:47 -0300

    Fix formatRequestDate

--- confinder.web/src/utils/formatRequestDate.ts
+++ confinder.web/src/utils/formatRequestDate.ts
@@ -1,3 +1,5 @@
-export const formatRequestDate = (date?: Date) => {
-  return date?.toISOString().substring(0, 10);
+export const formatRequestDate = (d?: Date): string | undefined => {
+  return d
+    ? new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate())).toISOString().substring(0, 10)
+    : undefined;
 };
